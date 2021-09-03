fbf44a5443be3ac456a7647ade447a143cdcce7f
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 17:38:50 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -21,15 +21,17 @@ async def on_ready():
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
 
-    while True:
-                print(time)
-                time = datetime.datetime.now().strftime("%H:%M")
-                sleep(2)
+    while time != "19:56":
+            print(time)
+            time = datetime.datetime.now().strftime("%H:%M")
+            sleep(2)
 
-                if day.weekday() == 4 and time == "20:37":
-                        embed = Embed(title = "É Sexta Feira Meus Bacanos!",description = "SEXTA DOS CRIA PORRA",colour = colour.Colour.red())
-                        await channel.send(embed = embed)
-                        await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
-                        await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
+    if day.weekday() == 4 and time == "19:56":
+        embed = Embed(title = "É Sexta Feira Meus Bacanos!",description = "SEXTA DOS CRIA PORRA",colour = colour.Colour.red())
+        await channel.send(embed = embed)
+        await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
+        await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
+        
+
 
 client.run(os.environ['DISCORD_TOKEN'])
