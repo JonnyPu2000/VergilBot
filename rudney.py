f64c77f28000dd770705986ba0cbccb2518dc3a1
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 15:59:39 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -12,27 +12,20 @@ day = datetime.datetime.today()
 @client.event
 async def on_ready():
     print("Inicializado")
-    
-''''''
-@client.event
-async def posta():
+
 
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
 
-    while time != "18:58":
+    while time != "19:02":
             print(time)
             time = datetime.datetime.now().strftime("%H:%M")
             sleep(2)
 
-    if day.weekday() == 4 and time == "18:58":
+    if day.weekday() == 4 and time == "19:02":
         embedVar = discord.Embed(title="Sexta dos CRIA", description="É sexta feira meus bacanos!!!", color=0x00ff00)
         embedVar.add_field(name="Sexta dos cria", value="https://www.youtube.com/watch?v=pCTSdupScwA", inline=False)
         embedVar.add_field(name="Sherekão", value="https://www.youtube.com/watch?v=HJY9dnDLgEQ", inline=False)
         await message.channel.send(embed=embedVar)
-
-    
-
-
 
 client.run(os.environ['DISCORD_TOKEN'])
