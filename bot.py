0fa693669947a8195fb23a3eef070ca348417254
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 12:24:35 -0300

    teste

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import datetime
+import os
 
 client = commands.Bot(command_prefix = '.')
 day = datetime.datetime.today()
@@ -8,6 +9,7 @@ day = datetime.datetime.today()
 
 @client.event
 async def on_ready():
+    print("Idosa Peladinha")
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
     while time != "12:30":
@@ -18,4 +20,4 @@ async def on_ready():
 
 
 
-client.run('ODgzMzI3OTUwMDgxMDk3NzI5.YTIVQg.chXPKvfzqCtgR_aBHixu9FJl3Wg')
+client.run(os.environ['DISCORD_TOKEN'])
