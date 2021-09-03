c5c75b003194de05be47a7f873ece81612690efa
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 15:40:21 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -16,29 +16,31 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!hello'):
-        embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+    if message.content.startswith('$Cria'):
+        embedVar = discord.Embed(title="Sexta dos CRIA", description="É sexta feira meus bacanos!!!", color=0x00ff00)
         embedVar.add_field(name="Field1", value="hi", inline=False)
         embedVar.add_field(name="Field2", value="hi2", inline=False)
         await message.channel.send(embed=embedVar)
 
+    channel = client.get_channel(645698417544265769)
+    time = datetime.datetime.now().strftime("%H:%M")
+    
+    '''
+    while time != "18:09":
+            print(time)
+            time = datetime.datetime.now().strftime("%H:%M")
+            sleep(1.5)'''
 
-
-    #channel = client.get_channel(645698417544265769)
-    #time = datetime.datetime.now().strftime("%H:%M")
-    #while time != "18:09":
-     #       print(time)
-      #      time = datetime.datetime.now().strftime("%H:%M")
-       #     sleep(1.5)
-
-    #if day.weekday() == 4 and time >= "18:09":
-     #       await channel.send(file)
-      #      await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")'''
+    if day.weekday() == 4:
+        embedVar = discord.Embed(title="Sexta dos CRIA", description="É sexta feira meus bacanos!!!", color=0x00ff00)
+        embedVar.add_field(name="Field1", value="hi", inline=False)
+        embedVar.add_field(name="Field2", value="hi2", inline=False)
+        await message.channel.send(embed=embedVar)
     
 
     
 
 
 
-#client.run(os.environ['DISCORD_TOKEN'])
+client.run(os.environ['DISCORD_TOKEN'])
 client.run('ODgzMzI3OTUwMDgxMDk3NzI5.YTIVQg.chXPKvfzqCtgR_aBHixu9FJl3Wg')
