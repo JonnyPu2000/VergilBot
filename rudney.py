c6e21cfae26a8aa574e0f825a87b39b7ac7e7b3e
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 16:51:57 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -21,14 +21,16 @@ async def on_ready():
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
 
-    while time != "19:50":
+    while time != "19:53":
             print(time)
             time = datetime.datetime.now().strftime("%H:%M")
             sleep(2)
 
-    if day.weekday() == 4 and time == "19:50":
-        await channel.send(File("./Assets/criaSexta.mp4"))
-        await channel.send(File("./Assets/shrekSexta.mp4"))
+    if day.weekday() == 4 and time == "19:453":
+        embed = Embed(title = "É Sexta Feira Meus Bacanos!",description = "SEXTA DOS CRIA PORRA",colour = colour.Colour.red())
+        await channel.send(embed = embed)
+        await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
+        await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
         
 
 
