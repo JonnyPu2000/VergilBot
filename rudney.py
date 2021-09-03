de2ac43899feb9c81a9f1bbd03d5a798a1a318cc
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 12:37:28 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -12,9 +12,9 @@ async def on_ready():
     print("Idosa Peladinha")
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
-    while time != "12:30":
+    while time != "12:40":
         time = datetime.datetime.now().strftime("%H:%M")
-    if day.weekday() == 4 and time == "12:30":
+    if day.weekday() == 4 and time == "12:40":
         await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
         await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
 
