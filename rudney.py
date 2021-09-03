32d63d4c115efb2a920aba0748f6873948e46a1b
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 13:09:19 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -2,11 +2,10 @@ import discord
 from discord.ext import commands
 import datetime
 import os
-import time as tm
+from time import sleep
 
 client = commands.Bot(command_prefix = '.')
 day = datetime.datetime.today()
-
 
 
 @client.event
@@ -14,18 +13,12 @@ async def on_ready():
     print("Idosa Peladinha")
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
-
-    for i in range(0,60):
-        time = datetime.datetime.now().strftime("%H:%M")
+    while time != "13:11":
         print(time)
-        if time == "13:07":
-            print("FOI")
-            break
-        tm.sleep(5)
-    print("SAIU") 
+        time = datetime.datetime.now().strftime("%H:%M")
+        sleep(1)
 
-    if day.weekday() == 4 and time == "13:08":
-        print("ENTROU")
+    if day.weekday() == 4 and time == "13:11":
         await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
         await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
 
