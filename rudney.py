edd7339abbddb06bb15da7b4dd01ea4fbde689ed
João Paulo Sillos Pereira <57176998+JonnyPu2000@users.noreply.github.com>
2021-09-03 12:56:35 -0300

    Update rudney.py

--- rudney.py
+++ rudney.py
@@ -14,12 +14,15 @@ async def on_ready():
     channel = client.get_channel(645698417544265769)
     time = datetime.datetime.now().strftime("%H:%M")
     for i in range(0,60):
+        if time == "12:57":
+            break
         print(time)
         time = datetime.datetime.now().strftime("%H:%M")
-        tm.sleep(1)
-        if time == "12:55":
-            break
-    if day.weekday() == 4 and time == "12:55":
+        tm.sleep(5)
+    print("SAIU") 
+      
+    if day.weekday() == 4 and time == "12:58":
+        print("ENTROU")
         await channel.send("https://www.youtube.com/watch?v=pCTSdupScwA")
         await channel.send("https://www.youtube.com/watch?v=8GX9--xhf_A")
 
